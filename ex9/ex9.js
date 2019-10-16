b110f1f34c8019b8caf649b4681f7b68c2f10b6a
trenddreamer <trenddreamer@gmail.com>
2019-10-16 21:15:05 +0200

    ex9

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
   var $btn = $("#btn"),
-    $list = $("#list");
+    $list = $("#list"),
+    clicks = ASQ.csp.chan(),
+    msgs = ASQ.csp.chan(),
+    queueClick;
 
-  $btn.click(function(evt) {
-    process1(evt);
-  }),
-    ASQ().runner(
-      ASQ.csp.go(function* process1(ch) {
-        yield ASQ.csp.put(ch, "Click");
-      }),
-      ASQ.csp.go(function* process2(ch) {
-        yield ASQ.csp.take(ch);
-        console.log("Clicked!");
-      })
-    );
+  $btn.click(listenToClicks);
+
+  function listenToClicks(evt) {
+    if (!queueClick) {
+      queueClick = ASQ.csp.putAsync(clicks, evt);
+      queueClick.then(function() {
+        queueClick = null;
+      });
+    }
+  }
+  ASQ().runner(ASQ.csp.go(singleClicks), ASQ.csp.go(logClicks));
+
+  function* singleClicks() {
+    while (true) {
+      yield ASQ.csp.take(ASQ.csp.timeout(1000));
+      yield ASQ.csp.take(clicks);
+      yield ASQ.csp.put(msgs, "Clicked!");
+    }
+  }
+
+  function* logClicks() {
+    while (true) {
+      var msg = yield ASQ.csp.take(msgs);
+      $list.append($("<div>" + msg + "</div>"));
+    }
+  }
 
   // TODO: setup sampling go-routine and
   // channel, populate $list
