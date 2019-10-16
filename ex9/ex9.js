272db95ceada469525327e69f6ce1e06fc7743c5
Barry <trenddreamer@gmail.com>
2019-10-16 16:30:45 +0200

    ex9

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -1,11 +1,20 @@
-$(document).ready(function(){
-	var $btn = $("#btn"),
-		$list = $("#list");
+$(document).ready(function() {
+  var $btn = $("#btn"),
+    $list = $("#list");
 
-	$btn.click(/* TODO */);
+  $btn.click(function(evt) {
+    process1(evt);
+  }),
+    ASQ().runner(
+      ASQ.csp.go(function* process1(ch) {
+        yield ASQ.csp.put(ch, "Click");
+      }),
+      ASQ.csp.go(function* process2(ch) {
+        yield ASQ.csp.take(ch);
+        console.log("Clicked!");
+      })
+    );
 
-	// Hint: ASQ().runner( .. )
-
-	// TODO: setup sampling go-routine and
-	// channel, populate $list
+  // TODO: setup sampling go-routine and
+  // channel, populate $list
 });
