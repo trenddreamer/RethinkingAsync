43e5bad32d96e754f453920c105e9717a7d9acbf
Barry <trenddreamer@gmail.com>
2019-10-16 15:04:18 +0200

    ex8

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,10 +1,28 @@
-$(document).ready(function(){
-	var $btn = $("#btn"),
-		$list = $("#list");
+$(document).ready(function() {
+  var $btn = $("#btn"),
+    $list = $("#list"),
+    rsq1 = ASQ.react.of(),
+    rsq2 = ASQ.react.of(),
+    latest;
 
-	$btn.click(function(evt){
-		// TODO
-	});
+  $btn.click(function(evt) {
+    rsq1.push(evt);
+  });
 
-	// TODO: setup sampled sequence, populate $list
+  setInterval(function() {
+    if (latest) {
+      rsq2.push();
+      latest = null;
+    }
+  }, 1000);
+
+  rsq1.val(function(evt) {
+    latest = evt;
+  });
+
+  rsq2.val(function(v) {
+    console.log("Clicked!");
+  });
+
+  // TODO: setup sampled sequence, populate $list
 });
